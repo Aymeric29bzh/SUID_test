bf16a97a6511d1596de4c21cb5582aa3ced0c9e2
Aymeric29bzh <38043270+Aymeric29bzh@users.noreply.github.com>
2020-01-03 13:50:46 +0100

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,6 +1,7 @@
 #Téléchargement des scripts :
 git clone https://github.com/Aymeric29bzh/SUID_test.git SUID_VERIF
 #Instruction cron :
+echo "0 *     * * *   root    sh /root/projetbdd/script2.sh" >> /etc/crontab
 
 
 
