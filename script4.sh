f574d6ef0cc9879bd5f2efb18fc5a8f8c7acaac5
Aymeric29bzh <38043270+Aymeric29bzh@users.noreply.github.com>
2020-01-05 18:30:25 +0100

    Update script4.sh

--- script4.sh
+++ script4.sh
@@ -1,5 +1,5 @@
 #Téléchargement des scripts :
-git clone https://github.com/Aymeric29bzh/SUID_test.git projetbdd
+git clone https://github.com/Aymeric29bzh/SUID_test.git /root/projetbdd
 #Instruction cron :
 echo "0 *     * * *   root    sh /root/projetbdd/script2.sh" >> /etc/crontab
 
